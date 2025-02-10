53890cc9d7b1db98326395fd99bccd0aab6ed539
devdoud <bouraimawadoud@gmail.com>
2025-02-10 12:33:31 +0100

    add comment to the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ export function Header() {
     return (
         <div className="bg-[#FFC146] sm:p-8 p-4 flex justify-between items-center shadow-md sm:h-16 w-full">
             <img src="src/assets/images/logo_1.jpg" alt="Logo Benin Restoo" className="sm:w-[42px] w-[26px]" />
+            //Voici les differentes rebriques de notre petit site web
             <ul className='sm:flex hidden'>
                 <li className="mr-3">Products</li>
                 <li className="mr-3">Menus</li>
